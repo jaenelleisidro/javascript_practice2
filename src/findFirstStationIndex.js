310333a00898c7c16f0564f00c58afe4480a2454
jay.isidro <jaenelleisidro@gmail.com>
2023-02-12 09:06:43 +0800

    using basic for loop

--- src/findFirstStationIndex.js
+++ src/findFirstStationIndex.js
@@ -1,5 +1,23 @@
 module.exports = (strArr)=>{
     //add logic here
+    if(!Array.isArray(strArr)){throw Error("parameter must be a string");}
 
+    let numberOfStation=parseInt(strArr[0])||0;
+    let currentGas=0;
+    let answer=1;
+    for (let i = 1; i <= numberOfStation; i++) {
+        const station = strArr[i];
+        if(!station){continue;};
+        const [gas,gasNeededNext]=station.split(":");
+        currentGas=currentGas+gas-gasNeededNext;
+        if(currentGas<0){
+            answer=i+1;
+            currentGas=0;
+        }
+
+    }
+    if(answer>numberOfStation){
+        return "impossible";
+    }
     return answer;
 }
